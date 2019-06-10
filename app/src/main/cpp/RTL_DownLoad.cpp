ef69fb75c2e5c8ebba09fc9a0930638f3bf7b072
AivenLau <aivenlau@126.com>
2019-06-10 10:03:18 +0800

    release

---
+++ app/src/main/cpp/RTL_DownLoad.cpp
@@ -0,0 +1,5 @@
+//
+// Created by AivenLau on 2019-05-13.
+//
+
+#include "RTL_DownLoad.h"
---
+++ app/src/main/cpp/RTL_DownLoad.h
@@ -0,0 +1,14 @@
+//
+// Created by AivenLau on 2019-05-13.
+//
+
+#ifndef SYMADEMO_ANDROID_RTL_DOWNLOAD_H
+#define SYMADEMO_ANDROID_RTL_DOWNLOAD_H
+
+
+class RTL_DownLoad {
+
+};
+
+
+#endif //SYMADEMO_ANDROID_RTL_DOWNLOAD_H
